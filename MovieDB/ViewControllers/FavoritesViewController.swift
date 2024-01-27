342f5eeb2c9d51e91c02747fd1531d11d7323e2a
pratyushajog <65998064+pratyushajog@users.noreply.github.com>
2024-01-26 16:03:57 -0800

    Initial setup with tab bar controller

---
+++ MovieDB/ViewControllers/FavoritesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesViewController.swift
+//  MovieDB
+//
+//  Created by Pratyusha Joginipally on 1/26/24.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      view.backgroundColor = .systemRed
+    }
+}
---
+++ MovieDB/ViewControllers/MovieListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListViewController.swift
+//  MovieDB
+//
+//  Created by Pratyusha Joginipally on 1/26/24.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      view.backgroundColor = .systemBlue
+    }
+
+}
